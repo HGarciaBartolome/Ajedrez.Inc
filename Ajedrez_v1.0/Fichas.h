36d91acae8ecdaf81a79479b0f3174dca67815cf
HGarciaBartolome <64076348+HGarciaBartolome@users.noreply.github.com>
2022-05-01 15:08:44 +0200

    Creacion de Classes inciales

---
+++ Ajedrez_v1.0/Fichas.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Vector2D.h"
+class Fichas {
+public:
+	Fichas();
+
+private:
+	Vector2D Posicion;//Posicion de la pieza
+	bool color;//Color de la pieza 0=Negra 1=Blanca
+
+};
+
+class Peon :public Fichas {
+public:
+
+private:
+
+};
