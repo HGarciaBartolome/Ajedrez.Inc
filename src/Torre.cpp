fd0524bc5bd561f05404805f00e9f4d5fffcffc2
HGarciaBartolome <h.garciab@alumnos.upm.es>
2022-05-03 18:41:29 +0200

    cambio

--- src/Torre.cpp
+++ src/Torre.cpp
@@ -1,10 +1,10 @@
 
 #include "Torre.h"
-#include "freeglut.h"
+//#include "freeglut.h"
 #include "Piezas.h"
 
 void Torre::Dibuja()
-{
+{/*
 	glEnable(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/fondo.png").id);
 	glDisable(GL_LIGHTING);
@@ -16,7 +16,7 @@ void Torre::Dibuja()
 	glTexCoord2d(0, 0); glVertex2f(-10, 15);
 	glEnd();
 	glEnable(GL_LIGHTING);
-	glDisable(GL_TEXTURE_2D);
+	glDisable(GL_TEXTURE_2D);*/
 }
 
 void Torre::mueve(Vector2D)
