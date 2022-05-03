fdb1ebc423f34d7538dcf902e3abdb686c5e5478
HGarciaBartolome <64076348+HGarciaBartolome@users.noreply.github.com>
2022-05-03 16:03:00 +0200

    Comienzo

---
+++ src/Ajedrez.cpp
@@ -0,0 +1,41 @@
+#include <ETSIDI.h>
+#include <stdio.h>     
+#include "glut.h"
+#include "Mundo.h"
+
+Mundo mundo;
+
+void OnDraw(void); //esta funcion sera llamada para dibujar
+void OnTimer(int value); //esta funcion sera llamada cuando transcurra una temporizacion
+void OnKeyboardDown(unsigned char key, int x, int y); //cuando se pulse una tecla
+void onMouse(int button, int state, int x, int y);
+
+int main(int argc, char* argv[])
+{
+
+
+
+}
+
+void OnDraw(void) {
+
+	
+
+}
+
+void OnKeyboardDown(unsigned char key, int x_t, int y_t)
+{
+
+
+}
+
+void OnTimer(int value)
+{
+
+
+}
+
+void onMouse(int button, int state, int x, int y)
+{
+
+}
---
+++ src/Vector2D.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class Vector2D {
+public:
+	float x, y;
+	Vector2D():x(0), y(0) {}
+};
